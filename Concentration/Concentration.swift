426cd6629169f0974dc9ebdc03b85eab5ddef2f7
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-12-12 11:11:12 -0600

    Implement data driven code'

---
+++ Concentration/Concentration.swift
@@ -0,0 +1,11 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Dumitru Gutu on 12/12/17.
+//  Copyright © 2017 Dumitru Gutu. All rights reserved.
+//
+
+import Foundation
+
+
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -11,25 +11,24 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var flipCountLabel: UILabel!
-    
-    var flipCount = 0 {
-        didSet {
-            flipCountLabel.text = "Flips: \(flipCount)"
-        }
-    }
-    
     @IBOutlet var cardButtons: [UIButton]!
     
+    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
+    
+    
+    var flipCount = 0 { didSet { flipCountLabel.text = "Flips: \(flipCount)" } }
+
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
-        let cardNumber = cardButtons.index(of: sender)
-        print("cardNumber: \(String(describing: cardNumber))")
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
     }
     
     func flipCard(withEmoji emoji: String, on button: UIButton ) {
         if button.currentTitle == emoji {
             button.setTitle("", for: .normal)
-            button.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
         } else {
             button.setTitle(emoji, for: .normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
