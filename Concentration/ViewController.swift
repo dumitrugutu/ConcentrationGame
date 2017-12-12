eedeb883d54b10751d9d02ab1135866e37454b8b
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-12-12 10:42:34 -0600

    Add initial cards

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        flipCard(withEmoji: "👻", on: sender)
     }
-
-
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton ) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
 }
-
